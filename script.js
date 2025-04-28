95f3febfacdcfce9c556b63202571ebeb4c2cd83
Kakauanid <kauanpsn15a@gmail.com>
2025-04-28 20:57:05 -0300

    melhorando...

---
+++ script.js
@@ -0,0 +1,54 @@
+
+function entrar() {
+    var username = document.getElementById('username').value.trim();
+    var password = document.getElementById('password').value.trim();
+    
+    if (username && password){
+        localStorage.setItem('logado', 'true');
+
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('principal').style.display = 'block';
+    } else {
+        alert('Por favor, preencha com Nome e Sobrenome.');
+    }
+}
+
+function mostrarSenha() {
+    var senha = document.getElementById('password');
+
+    if (senha.type === 'password') {
+        senha.type = "text";
+    } else {
+        senha.type = "password";
+    }
+}
+
+const menuFilmesBtn = document.getElementById('menu-filmes-btn');
+const menuFilmes = document.getElementById('menu-filmes');
+
+const menuSeriesBtn = document.getElementById('menu-series-btn');
+const menuSeries = document.getElementById('menu-series');
+
+const menuAnimesBtn = document.getElementById('menu-animes-btn');
+const menuAnimes = document.getElementById('menu-animes');
+
+// Filmes
+menuFilmesBtn.addEventListener('click', () => {
+    menuFilmes.style.display = menuFilmes.style.display === 'block' ? 'none' : 'block';
+    menuSeries.style.display = 'none';
+    menuAnimes.style.display = 'none';
+});
+
+// Séries
+menuSeriesBtn.addEventListener('click', () => {
+    menuSeries.style.display = menuSeries.style.display === 'block' ? 'none' : 'block';
+    menuFilmes.style.display = 'none';
+    menuAnimes.style.display = 'none';
+});
+
+// Animes
+menuAnimesBtn.addEventListener('click', () => {
+    menuAnimes.style.display = menuAnimes.style.display === 'block' ? 'none' : 'block';
+    menuFilmes.style.display = 'none';
+    menuSeries.style.display = 'none';
+});
